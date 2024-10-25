1e47f5820f2a4414bca089d9b6bdd542e3807932
Lennart Kloock <lennart.kloock@protonmail.com>
2024-10-25 10:41:11 +0200

    fix: extension auth

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,5 +1,6 @@
 import { RouteRecordRaw, createRouter, createWebHistory } from "vue-router";
 import { AdminRoute } from "./admin.route";
+import { ExtensionAuthRoute } from "./auth.route";
 import { CallbackRoute } from "./callback.route";
 import { EmoteSetRoute } from "./emote-sets.route";
 import { EmotesRoute } from "./emotes.route";
@@ -19,6 +20,7 @@ const routes: Array<RouteRecordRaw> = [
 	HelpRoute,
 	AdminRoute,
 	...CallbackRoute,
+	ExtensionAuthRoute,
 	{
 		path: "/:pathMatch(.*)*",
 		name: "Not Found",
